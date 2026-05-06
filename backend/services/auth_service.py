afcf568c48a89a8525f74d74244a1628d5944a1e
Agustin Mazzoni <mazzoni.agustine@gmail.com>
2026-05-06 18:39:16 -0300

    Se realiza una mejora de legibilidad.

--- backend/services/auth_service.py
+++ backend/services/auth_service.py
@@ -1,38 +1,38 @@
 from fastapi import HTTPException
 from database import supabase
 
+#Aplicacion de las reglas de negocio.
 
-class AuthService:
-    @staticmethod
-    def register_user(data):
-        try: 
-            auth_response = supabase.auth.sign_up({
-                "email": data.email,
-                "password": data.password
-            })
+def register_user(data):
+    try: 
+        auth_response = supabase.auth.sign_up({
+            "email": data.email,
+            "password": data.password
+        })
             
-            if auth_response:
-                supabase.table('users').insert({
-                    'id': auth_response.user.id,
-                    'name': data.name,
-                    'surname': data.surname,
-                    'email': data.email,
-                    'dni': data.dni,
-                    'rol': 'NO_ABONADO',
-                    'physical_certificate': 'Pendiente',
-                    'account_status': 'Activa'
-                    #Url del dni?
-                }).execute()
-                return {"Mensaje": "Usuario registrado exitosamente. Queda pendiente de verificación del apto físico."}
-        except Exception as e:
-            raise HTTPException(status_code=400, detail=f'Error en el registro del usuario: {str(e)}')
-    @staticmethod    
-    def login_user(email,password):
-        try:
-            response = supabase.auth.sign_in_with_password({
-                'email': email,
-                'password': password
-            })
-            return {'Mensaje:': 'Usuario autenticado exitosamente', 'Token': response.session.access_token}
-        except Exception:
-            raise HTTPException(status_code=401, detail='Error en las credenciales de inicio de sesión.')
+        if auth_response:
+            supabase.table('users').insert({
+                'id': auth_response.user.id,
+                'name': data.name,
+                'surname': data.surname,
+                'email': data.email,
+                'dni': data.dni,
+                'rol': 'NO_ABONADO',
+                'physical_certificate': 'Pendiente',
+                'account_status': 'Activa'
+                #Url del dni?
+                    
+            }).execute()
+            return {"Mensaje": "Usuario registrado exitosamente. Queda pendiente de verificación del apto físico."}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f'Error en el registro del usuario: {str(e)}')
+        
+def login_user(email,password):
+    try:
+        response = supabase.auth.sign_in_with_password({
+            'email': email,
+            'password': password
+        })
+        return {'Mensaje:': 'Usuario autenticado exitosamente', 'Token': response.session.access_token}
+    except Exception:
+        raise HTTPException(status_code=401, detail='Error en las credenciales de inicio de sesión.')
