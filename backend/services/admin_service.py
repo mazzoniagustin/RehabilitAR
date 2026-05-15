d397609f4dd865f50033f6b01ec18f233375f2f0
Marcos Agustin Mazzoni <mazzoni.agustine@gmail.com>
2026-05-15 18:22:09 -0300

    Fix: Se solucionan bugs de exceptions y creacion de usuarios.

--- backend/services/admin_service.py
+++ backend/services/admin_service.py
@@ -1,37 +1,49 @@
 from fastapi import HTTPException
+from utils.password_utils import random_password
+from utils.permissions import check_user_existance
 from database import supabase
-from utils import passord_utils, permissions
-
 #Aplicacion de las reglas de negocio.
 
 def register_user_by_staff(data):
     try:
+        check_user_existance(data.email)
         password = random_password()
         auth_response = supabase.auth.admin.create_user({
             'email': data.email, 
             'password': password
         })
             
-        if auth_response:
-            supabase.table('users').insert({
-                'id': auth_response.user.id,
-                'name': data.name,
-                'surname': data.surname,
-                'email': data.email,
-                'dni': data.dni,
-                'rol': 'NO_ABONADO',
-                'physical_certificate': 'Pendiente',
-                'account_status': 'Activa'
-            }).execute()
+            
+        if not auth_response.user:
+            raise HTTPException(status_code=400, detail='Error en el registro del usuario.')
+
+        user_id = auth_response.user.id
+        
+        supabase.table('users').insert({
+            'id': user_id,
+            'name': data.name,
+            'surname': data.surname,
+            'email': data.email,
+            'dni': data.dni,
+            'rol': 'NO_ABONADO',
+            'physical_certificate': 'Pendiente',
+            #'physical_certificate_url': data.physical_certificate_url,
+            'account_status': 'ACTIVA',
+            'failed_attempts': 0                
+        }).execute()
                 
             #Enviar mail con la contraseña del usuario
                 
-            return {"Mensaje": "Usuario registrado exitosamente."}
+        return {"Mensaje": "Usuario registrado exitosamente."}
+    except HTTPException:
+        raise
     except Exception as e:
         raise HTTPException(status_code=400, detail=f'Error en el registro del usuario.') 
         
 def register_employee_by_admin(data):
     try:
+        
+        check_user_existance(data.email)
 
         if data.rol in ['RECEPCIONISTA', 'PROFESOR'] and not data.specialty:
             raise HTTPException(status_code=400, detail='La especialidad es obligatoria para recepcionistas y profesores.')
@@ -42,30 +54,37 @@ def register_employee_by_admin(data):
             'password': password
         })
 
-        if response:
-            supabase.table('users').insert({
-                'id': response.user.id,
-                'name': data.name,
-                'surname': data.surname,
-                'email': data.email,
-                'dni': data.dni,
-                'rol': data.rol,
-                'account_status': 'Activa',
-                'specialty': data.specialty
-            }).execute()
+        if not response:
+            raise HTTPException(status_code=400, detail='Error en el registro del empleado.')
+        user_id = response.user.id
+        
+        supabase.table('users').insert({
+            'id': user_id,
+            'name': data.name,
+            'surname': data.surname,
+            'email': data.email,
+            'dni': data.dni,
+            'rol': data.rol,
+            'account_status': 'ACTIVA',
+            'specialty': data.specialty,
+            'failed_attempts': 0
+        }).execute()
 
-            return {"Mensaje": "Empleado registrado exitosamente."}
+        return {"Mensaje": "Empleado registrado exitosamente."}
+    
+    except HTTPException:
+        raise
     except Exception as e:
-        raise HTTPException(status_code=400, detail=f'Error en el registro del empleado.')
+        raise HTTPException(status_code=400, detail=f'Error en el registro del empleado. {str(e)}')
 
 
 def approve_certificate(data):
     try:
-        response = supabase.table('users').select('physical_certificate').eq('id', data.id).single().execute()
+        response = supabase.table('users').select('physical_certificate').eq('id', data.id).execute()
             
         if not response.data:
             raise HTTPException(status_code=404, detail='Usuario no encontrado.')
-        if response.data['physical_certificate'] == 'Aprobado':
+        if response.data[0]['physical_certificate'] == 'Aprobado':
             raise HTTPException(status_code=400, detail='El apto físico ya ha sido aprobado.')
             
         supabase.table('users').update({'physical_certificate': 'Aprobado'}).eq('id', data.id).execute()
@@ -74,20 +93,22 @@ def approve_certificate(data):
     except Exception as e:
         raise HTTPException(status_code=400, detail=f'Error al aprobar el certificado físico.')
     
-def reject_certificate(data):
+def reject_certificate(data, reason):
     try:
-        response = supabase.table('users').select('physical_certificate').eq('id', data.id).single().execute()
+        reason_text = reason.reason if hasattr(reason, 'reason') else reason
+        response = supabase.table('users').select('physical_certificate').eq('id', data.id).execute()
             
         if not response.data:
             raise HTTPException(status_code=404, detail='Usuario no encontrado.')
-        if response.data['physical_certificate'] == 'Rechazado':
+        if response.data[0]['physical_certificate'] == 'Rechazado':
             raise HTTPException(status_code=400, detail='El apto físico ya ha sido rechazado.')
             
         supabase.table('users').update({
             'physical_certificate': 'Rechazado',
-            'physical_rejection_reason': data.reason}).eq('id', data.id).execute()
+            'physical_rejection_reason': reason_text}).eq('id', data.id).execute()
         return {'Mensaje': 'Apto físico rechazado.'}
-        
+
+    except HTTPException:
+        raise   
     except Exception as e:
         raise HTTPException(status_code=400, detail=f'Error al rechazar el certificado físico.')
-        
