b3ec5f853e0664cff8a432c314cff9267683b04f
Marcos Agustin Mazzoni <mazzoni.agustine@gmail.com>
2026-05-15 18:24:47 -0300

    feat: Se agregan los routers para cambiar y recuperar la contraseña.

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,13 +1,13 @@
-from fastapi import APIRouter, HTTPException
-from schemes.user_scheme import UserRegister, UserLogin, UserRegisterByStaff, ApproveCertificate, RejectCertificate, EmployeeRegisterByAdmin, ChangePassword
-from services import auth_service, admin_service
-from utils.permissions import check_permission
+from fastapi import APIRouter, Depends
+from services import auth_service, user_service
+from schemes.user_scheme import RecoverPassword, UserRegister, UserLogin, ChangePassword
+from utils.permissions import get_current_user
 
 
 #Toma las requests y retorna las responses / respuestas
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
-routerStaff = APIRouter(prefix="/staff", tags=["staff"])
+
 
 @router.post("/register")
 def register(data: UserRegister):
@@ -17,33 +17,17 @@ def register(data: UserRegister):
 def login(data: UserLogin):
     return auth_service.login_user(data.email, data.password)
 
-@routerStaff.post('/register')
-def register_by_staff(data: UserRegisterByStaff):
-    if not check_permission(data.staff_id, ['ADMINISTRATIVO', 'RECEPCIONISTA']):
-        raise HTTPException(status_code=403, detail=f'Acceso denegado. Permisos insuficientes.')
-    return admin_service.register_user_by_staff(data)
-
-@routerStaff.post('/approve_certificate')
-def approve_certificate(data: ApproveCertificate):
-    if not check_permission(data.admin_id, ['ADMINISTRATIVO']):
-        raise HTTPException(status_code=403, detail=f'Acceso denegado. Permisos insuficientes.')
-    return admin_service.approve_certificate(data)
-
-@routerStaff.post('/reject_certificate')
-def reject_certificate(data: RejectCertificate):
-    if not check_permission(data.admin_id, ['ADMINISTRATIVO']):
-        raise HTTPException(status_code=403, detail=f'Acceso denegado. Permisos insuficientes.')
-    return admin_service.reject_certificate(data)
-
-@routerStaff.post('/register_employee')
-def register_employee_by_staff(data: EmployeeRegisterByAdmin):
-    if not check_permission(data.admin_id, ['ADMINISTRATIVO']):
-        raise HTTPException(status_code=403, detail=f'Acceso denegado. Permisos insuficientes.')
-    return admin_service.register_employee_by_admin(data)
 
 @router.post('/change_password')
-def change_password(data: ChangePassword):
-    return auth_service.change_password(data)
+def change_password(
+    data: ChangePassword,
+    current_user: dict = Depends(get_current_user)
+):
+    return user_service.change_password(current_user['id'], data)
+
+@router.post('/recover-password')
+def recover_password(data: RecoverPassword):
+    return auth_service.recover_password(data.email)
 
 @router.post('/logout')
 def log_out():
