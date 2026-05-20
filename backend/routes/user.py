ca7f1ff3e433643c744857406921528c480b612b
Marcos Agustin Mazzoni <mazzoni.agustine@gmail.com>
2026-05-20 02:57:42 -0300

    feature: Se agregan los routers de los USERS para manejo de sus cuentas.

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -25,3 +25,38 @@ def upload_certificate(
 ):
     return user_service.upload_certificate(user["id"], file)
 
+@routerUser.put('/me')
+def update_my_profile(
+    data: UserUpdate,
+    current_user: dict = Depends(get_current_user)
+):
+    update_dict = data.dict(exclude_unset=True)
+    return user_service.update_user_info(current_user['id'], update_dict)
+
+
+@routerUser.get('/public/{user_id}')
+def get_public_profile(user_id: str, current_user: dict = Depends(get_current_user)):
+    return user_service.show_user_public_info(user_id)
+
+@routerUser.get('/public-search')
+def public_search(
+    name: str = Query(None),
+    role: str = Query(None),
+    current_user: dict = Depends(get_current_user)
+):
+    return user_service.search_users_public(name=name, role=role)
+
+@routerUser.post('/request-unblock')
+def request_unblock(
+    data: ActionReason,
+    current_user: dict = Depends(get_current_user)
+):
+    return user_service.request_unblock(current_user['id'], data)
+
+@routerUser.get('/{user_id}')
+def get_user_profile(
+    user_id: str,
+    current_user: dict = Depends(check_permission(['ADMINISTRATIVO']))
+):
+    
+    return user_service.show_user_admin_info(user_id)
