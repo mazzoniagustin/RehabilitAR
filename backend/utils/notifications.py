ee297c344d52d7ef7e6a645c9d53eacc587e7bf2
Marcos Agustin Mazzoni <mazzoni.agustine@gmail.com>
2026-05-24 15:54:22 -0300

    Se agrega la funcion que permite el envío de notificaciones a los usuarios creados por un staff.

--- backend/utils/notifications.py
+++ backend/utils/notifications.py
@@ -7,17 +7,29 @@ acc_password = os.getenv('acc_password')
 def send_account_created_email(to_email: str, name: str, password: str):
     msg = EmailMessage()
     msg['Subject'] = 'Cuenta creada en RehabilitAR'
-    msg['From'] = 'RehabilitAR.faq@gmail.com'
+    msg['From'] = 'RehabilitAR <rehabilitar.faq@gmail.com>'
     msg['To'] = to_email
-    msg.set_content(f'Hola {name},\n\nTu cuenta ha sido creada exitosamente.
-    \n\nTu contraseña es: {password}.
-    \n\nRecordá cambiar tu contraseña a una de tu preferencia
-    \n\nSaludos,\nEl equipo de RehabilitAR')
+
+    msg.set_content(
+        f"""Hola {name},
+
+        Tu cuenta ha sido creada exitosamente.
+
+        Usuario: {to_email}
+        Contraseña temporal: {password}
+
+        ⚠️ Te recomendamos cambiar tu contraseña al ingresar.
+
+        Saludos,
+        Equipo RehabilitAR
+        """
+    )
 
     try:
-        with smtplib.SMTP('smtp.gmail.com', 456) as smtp:
-            smtp.login('RehabilitAR.faq@gmail.com', acc_password)
+        with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+            smtp.starttls() 
+            smtp.login('rehabilitar.faq@gmail.com', acc_password)
             smtp.send_message(msg)
-        
+
     except Exception as e:
         print(f'Error al enviar el correo: {e}')
