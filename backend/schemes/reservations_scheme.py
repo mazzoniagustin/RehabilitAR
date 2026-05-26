08d9043161c1d8063b689245ed588b49d2821958
LGalluzzi-1 <lucianogalluzzi.458@gmail.com>
2026-05-26 15:52:34 -0300

    Implementacion de reservas individuales y de abonados

---
+++ backend/schemes/reservations_scheme.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+from uuid import UUID
+
+class IndividualReservation(BaseModel):
+    class_id: UUID
+    payment_percentage: int
+
+class RegularReservation(BaseModel):
+    class_id: UUID
