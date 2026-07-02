6b774683f51f82953a760dfbdf005bb3f3a2b369
Marcos Agustin Mazzoni <mazzoni.agustine@gmail.com>
2026-07-02 01:08:29 -0300

    Se carga la auditoría.

--- frontend/dashboard.js
+++ frontend/dashboard.js
@@ -302,6 +302,10 @@ async function loadDashboard() {
 
   buildSidebar(u.rol);
   initNotifications();
+
+  if (typeof initAuditDashboard === 'function') {
+    window.initAuditDashboard();
+  }
 }
 
 
@@ -2695,6 +2699,13 @@ async function handleChangePassword() {
     document.getElementById('currentPw').value = '';
     document.getElementById('newPw').value = '';
     document.getElementById('confirmPw').value = '';
+    if (data.force_logout) {
+      localStorage.clear();
+      sessionStorage.clear();
+      setTimeout(() => {
+      window.location.href = 'http://localhost:8000/frontend/login.html';
+      }, 2500);
+    }
   } catch { showAlert('pwAlert', 'No se pudo conectar.'); }
 }
 
